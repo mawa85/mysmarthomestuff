e7c840e99f2bbf06405a384d221c4176bbb4b33f
mawa85 <mawa85@gmail.com>
2017-04-19 21:24:40 +0200

    Update sendtimetoknxbus.sh
    added #Sunday mapping: Linux=0 KNX=7

--- sendtimetoknxbus.sh
+++ sendtimetoknxbus.sh
@@ -11,9 +11,14 @@ HH=$(date +%H)
 MM=$(date +%M)
 SS=$(date +%S)
 
-p1=$((($DW*32)+$HH))
+#Sunday mapping: Linux=0 KNX=7
+if [ $DW = 0 ]; then
+   DW=7
+fi
 
-p1=`echo "obase=16; $p1" | bc`
+tmp1=$((($DW*32)+$HH))
+
+p1=`echo "obase=16; $tmp1" | bc`
 p2=`echo "obase=16; $MM" | bc`
 p3=`echo "obase=16; $SS" | bc`
 
