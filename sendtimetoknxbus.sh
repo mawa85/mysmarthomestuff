2b096a47fa970d4848f0e8ea78f31cedc8793034
mawa85 <mawa85@gmail.com>
2017-04-25 11:45:23 +0200

    Update sendtimetoknxbus.sh
    internet check and ntp update added

--- sendtimetoknxbus.sh
+++ sendtimetoknxbus.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-##
-##requirements: knxd with groupwrite
+##this script updates datetime s sson internet is available and sends the current timeto knx bus
+##requirements: knxd with groupwrite, internet access
 ##
 ##Input parameters:
 ## $1 IP of knx gateway
 ## $2 Group Address DPT 10.001 
 
+while true; do ping -c1 www.google.com > /dev/null && break; done
+
+ntpd -g
+
 DW=$(date +%u)
 HH=$(date +%H)
 MM=$(date +%M)
 SS=$(date +%S)
+
+HH=$((10#$HH))
+MM=$((10#$MM))
+SS=$((10#$SS))
+
 
 #Sunday mapping: Linux=0 KNX=7
 if [ $DW = 0 ]; then
